2ebe49ddffa563d5765f0d272be7fa3a63da77c2
Karam-a <81566216+Karam-a@users.noreply.github.com>
2022-12-19 16:49:44 +0100

    Generella förbättringar

--- src/mibg3/ErrorHant.java
+++ src/mibg3/ErrorHant.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Klassen är till för att hantera alla errormeddelanden. Vid ett eventuellt error påkallas en metod från denna klass. 
  */
 package mibg3;
 
